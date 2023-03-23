5758afa4ca49c3ae512e83865fa5a9353e719235
Kon09 <baske.19980916@gmail.com>
2023-03-23 11:09:49 +0900

    課題提出となります。

---
+++ HelloWorld/src/sample/selfIntroduction.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class selfIntroduction {
+
+	public static void main(String[] args) {
+		//氏名
+		String name = "近藤 祐生";
+		//年齢
+		int age = 24;
+		//趣味
+		String hobby = "ゲーム";
+		
+		/*
+		 * 自己紹介文
+		 */
+		
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
