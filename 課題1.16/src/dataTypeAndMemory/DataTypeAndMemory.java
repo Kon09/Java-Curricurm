0d85826a2c70da35272b6c109b4252234d135cfd
Kon09 <baske.19980916@gmail.com>
2023-03-31 16:09:23 +0900

    課題1.16の提出となります。

---
+++ 課題1.16/src/dataTypeAndMemory/DataTypeAndMemory.java
@@ -0,0 +1,38 @@
+package dataTypeAndMemory;
+
+public class DataTypeAndMemory {
+
+	public static void main(String[] args) {
+		// Q1：「A」という文字列を変数に代入してコンソールに出力してください。
+		String a;
+		a = "A";
+		System.out.println(a);
+
+		// Q2：「Hello Java」という文字列を変数に代入してコンソールに出力してください。
+		String hj;
+		hj = "Hello Java";
+		System.out.println(hj);
+			
+		// Q3：富士山の標高を整数で変数に代入してコンソールに出力してください。
+		int fuji_h;
+		fuji_h = 3776;
+		System.out.println(fuji_h);
+
+		// Q4：円周率を小数点第二位まで変数に代入してコンソールに出力してください。
+		double p;
+		p = 3.14;
+		System.out.println(p);
+				
+		// Q5：変数numsに「10, 100, 1000」が格納された配列を代入して、コンソールにメモリの番地が1のデータを出力してください。
+		int[] nums = {10, 100, 1000};
+		System.out.println(nums[1]);
+		
+		// Q6：Q5で作成した変数numsを変数bに代入し、その後変数bのメモリの番地が1のデータを「8」に上書きさせて
+		//     コンソールに変数numsと変数bのメモリの番地が1のデータを出力させて、変数nums値が変わることを確認してください。
+		int[] b = nums;
+		b[1] = 8;
+		System.out.println("nums[1]:" + nums[1]);
+		System.out.println("b[1]:" + b[1]);
+	}
+
+}
