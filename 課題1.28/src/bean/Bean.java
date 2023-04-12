6a1d12bcceb316926d4a9ee46e71dfcf0b989729
Kon09 <baske.19980916@gmail.com>
2023-04-12 21:51:32 +0900

    setterとgetterの処理を追加しました

---
+++ 課題1.28/src/bean/Bean.java
@@ -0,0 +1,14 @@
+package bean;
+
+public class Bean {
+	private String sName;
+	
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+	
+	public String getsName() {
+		return this.sName;
+	}
+
+}
--- 課題1.28/src/process/Process.java
+++ 課題1.28/src/process/Process.java
@@ -2,19 +2,24 @@ package process;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
+import bean.Bean;
 
 public class Process {
 //	コンストラクタの作成
 	public Process(){
 		System.out.println("コンソールに文字を入力してください");
 	}
+	
+//	インスタンス化
 	List<String[]> animal_list = new ArrayList<String[]> ();
+	Bean b = new Bean();
 	
 //	コンソールに入力、入力した内容を出力するメソッドの作成
 	public void disp() {
 		Scanner scan = new Scanner(System.in);
 		String line = scan.nextLine();
 		scan.close();
+		
 //		入力した内容をカンマ区切り
 		String[] animal = line.split(",");
 //		カンマで区切ったものをコロン区切りしてリストに入れる
@@ -30,22 +35,28 @@ public class Process {
 			System.out.println("速度：" + data[2] + "km/h");
 			switch(data[0]) {
 			case "ライオン":
-				System.out.println("学名：バンテラ　レオ");
+				b.setsName("バンテラ レオ");
+				System.out.println("学名：" + b.getsName());
 				break;
 			case "ゾウ":
-				System.out.println("学名：ロキソドンタ・サイクロティス");
+				b.setsName("ロキソドンタ・サイクロティス");
+				System.out.println("学名：" + b.getsName());
 				break;
 			case "パンダ":
-				System.out.println("学名：アイルロポダ・メラノレウカ");
+				b.setsName("アイルロポダ・メラノレウカ");
+				System.out.println("学名：" + b.getsName());
 				break;
 			case "チンパンジー":
-				System.out.println("学名：パン・トゥログロディテス");
+				b.setsName("パン・トゥログロディテス");
+				System.out.println("学名：" + b.getsName());
 				break;
 			case "シマウマ":
-				System.out.println("学名：チャップマンシマウマ");
+				b.setsName("チャップマンシマウマ");
+				System.out.println("学名：" + b.getsName());
 				break;
 			default:
-				System.out.println("学名：不明");
+				b.setsName("不明");
+				System.out.println("学名：" + b.getsName());
 				break;
 			}
 			System.out.println("");
