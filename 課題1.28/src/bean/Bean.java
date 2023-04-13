d13e3a8d29ac859401500fa3d966e92135bdf4f3
Kon09 <baske.19980916@gmail.com>
2023-04-14 01:20:08 +0900

    動物名、体長、速度をsetterとgetterで管理する処理を追加しました。

--- 課題1.28/src/bean/Bean.java
+++ 課題1.28/src/bean/Bean.java
@@ -1,13 +1,44 @@
 package bean;
 
 public class Bean {
-	private String sName;
+//	動物名と学名のフィールドを作成
+	private String animal_name, sName;
+//	体長と速度のフィールドを作成
+	private double animal_size;
+	private int animal_speed;
 	
-	public void setsName(String sName) {
-		this.sName = sName;
+//	動物名を編集、取得するメソッド
+	public void setAnimal_name(String animal_name) {
+		this.animal_name = animal_name;
 	}
 	
-	public String getsName() {
+	public String getAnimal_name() {
+		return this.animal_name;
+	}
+	
+//	体長を編集、取得するメソッド
+	public void setAnimal_size(double animal_size) {
+		this.animal_size = animal_size;
+	}
+	
+	public double getAnimal_size() {
+		return this.animal_size;
+	}
+	
+//	速度を編集、取得するメソッド
+	public void setAnimal_speed(int animal_speed) {
+		this.animal_speed = animal_speed;
+	}
+	
+	public int getAnimal_speed() {
+		return this.animal_speed;
+	}
+	
+//	学名を編集、取得するメソッド
+	public void setSName(String sName) {
+		this.sName = sName;
+	}
+	public String getSName() {
 		return this.sName;
 	}
 
--- 課題1.28/src/process/Process.java
+++ 課題1.28/src/process/Process.java
@@ -27,36 +27,48 @@ public class Process {
 			animal_list.add(str.split(":"));
 		}
 		
-//		リストの中身を出力
+//		出力処理メソッド
 		for(int i = 0, size = animal_list.size(); i < size; i++ ) {
+//			リストの中身を配列に格納
 			String[] data = animal_list.get(i);
-			System.out.println("動物名：" + data[0]);
-			System.out.println("体長：" + data[1] + "m");
-			System.out.println("速度：" + data[2] + "km/h");
+			
+//			体長と速度を各数値型に変換
+			double sizeDouble = Double.parseDouble(data[1]);
+			int speedInt = Integer.parseInt(data[2]);
+			
+//			動物名、体長、速度をsetterとgetterで管理して出力
+			b.setAnimal_name(data[0]);
+			System.out.println("動物名：" + b.getAnimal_name());
+			b.setAnimal_size(sizeDouble);
+			System.out.println("体長：" + b.getAnimal_size() + "m");
+			b.setAnimal_speed(speedInt);
+			System.out.println("速度：" + b.getAnimal_speed() + "km/h");
+			
+//			学名を条件分岐にて出力
 			switch(data[0]) {
 			case "ライオン":
-				b.setsName("バンテラ レオ");
-				System.out.println("学名：" + b.getsName());
+				b.setSName("バンテラ レオ");
+				System.out.println("学名：" + b.getSName());
 				break;
 			case "ゾウ":
-				b.setsName("ロキソドンタ・サイクロティス");
-				System.out.println("学名：" + b.getsName());
+				b.setSName("ロキソドンタ・サイクロティス");
+				System.out.println("学名：" + b.getSName());
 				break;
 			case "パンダ":
-				b.setsName("アイルロポダ・メラノレウカ");
-				System.out.println("学名：" + b.getsName());
+				b.setSName("アイルロポダ・メラノレウカ");
+				System.out.println("学名：" + b.getSName());
 				break;
 			case "チンパンジー":
-				b.setsName("パン・トゥログロディテス");
-				System.out.println("学名：" + b.getsName());
+				b.setSName("パン・トゥログロディテス");
+				System.out.println("学名：" + b.getSName());
 				break;
 			case "シマウマ":
-				b.setsName("チャップマンシマウマ");
-				System.out.println("学名：" + b.getsName());
+				b.setSName("チャップマンシマウマ");
+				System.out.println("学名：" + b.getSName());
 				break;
 			default:
-				b.setsName("不明");
-				System.out.println("学名：" + b.getsName());
+				b.setSName("不明");
+				System.out.println("学名：" + b.getSName());
 				break;
 			}
 			System.out.println("");
